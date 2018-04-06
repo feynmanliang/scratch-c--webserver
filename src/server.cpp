dd4d6995a8190f413c57fb682fcddca820badfed
Feynman Liang <feynman.liang@gmail.com>
2018-04-06 13:17:43 -0700

    Factor out server setup

--- src/server.cpp
+++ src/server.cpp
@@ -7,62 +7,71 @@
 #include <netdb.h>
 #include <netinet/in.h>
 
-#define PORTNO "3000"
-
 void error(const char *msg) {
     perror(msg);
     exit(1);
 }
 
-int main(int argc, char *argv[]) {
-    int sockfd, newsockfd;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_storage cli_addr;
-     int n;
+/**
+ * Sets up a TCP listener on specified port and returns the file descriptor.
+ */
+int listen(int port) {
+    int sockfd;
 
-      // `getaddrinfo`, does DNS resolution and sets up `addrinfo` struct
-      // since we're building a server we use NULL pointer for hostname and
-      // rely on AI_PASSIVE to fill it in
-     int status;
-     struct addrinfo hints, *servinfo;
-     memset(&hints, 0, sizeof hints);
-     hints.ai_family = AF_UNSPEC;     // IPv4 or IPv6 both work
-     hints.ai_socktype = SOCK_STREAM; // TCP stream socket
-     hints.ai_flags = AI_PASSIVE; // returned socket address structure intended for bind; fill in hostname
-     if ((status = getaddrinfo(NULL, PORTNO, &hints, &servinfo)) != 0) {
-         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-         exit(1);
-     }
+    // `getaddrinfo`, does DNS resolution and sets up `addrinfo` struct
+    // since we're building a server we use NULL pointer for hostname and
+    // rely on AI_PASSIVE to fill it in
+    int status;
+    struct addrinfo hints, *servinfo;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;     // IPv4 or IPv6 both work
+    hints.ai_socktype = SOCK_STREAM; // TCP stream socket
+    hints.ai_flags = AI_PASSIVE; // returned socket address structure intended for bind; fill in hostname
+    char portstr[8];
+    sprintf(portstr, "%i", port);
+    if ((status = getaddrinfo(NULL, portstr, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
 
-     // creates a socket
-     sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-     if (sockfd < 0)
+    // creates a socket
+    sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    if (sockfd < 0)
         error("ERROR opening socket");
 
-     // allow socket to be reused immediately
-     int yes = 1;
-     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1)
-         error("ERROR setting socket option");
+    // allow socket to be reused immediately
+    int yes = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1)
+        error("ERROR setting socket option");
 
-     // binds socket to localhost:3000 (specified in servinfo) and starts listening
-     if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) < 0)
-              error("ERROR on binding");
-     listen(sockfd, 0); // no backlog
+    // binds socket to localhost:3000 (specified in servinfo) and starts listening
+    if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) < 0)
+        error("ERROR on binding");
+    freeaddrinfo(servinfo);
+    listen(sockfd, 0); // no backlog
+    return sockfd;
+}
 
-     clilen = sizeof cli_addr;
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0)
-          error("ERROR on accept");
-     memset(buffer, 0, 256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
+int main(int argc, char *argv[]) {
+    int sockfd, newsockfd;
+    sockfd = listen(3000);
+    socklen_t clilen;
+    char buffer[256];
+    int n;
+    struct sockaddr_storage cli_addr;
 
-     freeaddrinfo(servinfo);
-     return 0;
+    clilen = sizeof cli_addr;
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0)
+        error("ERROR on accept");
+    memset(buffer, 0, 256);
+    // read() is the generic fd syscall, recv() is specific to sockets
+    n = recv(newsockfd, buffer, 255, 0);
+    if (n < 0) error("ERROR reading from socket");
+    printf("Here is the message: %s\n",buffer);
+    n = write(newsockfd,"I got your message",18);
+    if (n < 0) error("ERROR writing to socket");
+    close(newsockfd);
+    close(sockfd);
+    return 0;
 }
